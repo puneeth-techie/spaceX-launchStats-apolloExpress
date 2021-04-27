244e886ad4f77cf1cea957d81a5c7e7cc2c180b0
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-04-27 10:56:08 +0530

    Implemented apollo server.

---
+++ src/graphql/startApolloServer.js
@@ -0,0 +1,24 @@
+import { ApolloServer } from "apollo-server-express";
+import app from "../startup/app.js";
+import typeDefs from "./typeDefs.js";
+import resolvers from "./resolvers.js";
+
+const startApolloServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground:
+      process.env.NODE_ENV !== "development"
+        ? false
+        : {
+            settings: {
+              "request.credentials": "include",
+            },
+          },
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+export default startApolloServer;
